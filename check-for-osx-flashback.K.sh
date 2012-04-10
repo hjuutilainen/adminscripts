59ce2f6750687c08d7ac000ac5082d9b1b9bd941
Hannes Juutilainen <hjuutilainen@mac.com>
2012-04-10 15:06:30 +0300

    Added support for checking multiple browsers.

--- check-for-osx-flashback.K.sh
+++ check-for-osx-flashback.K.sh
@@ -10,52 +10,93 @@
 # Hannes Juutilainen, hjuutilainen@mac.com
 #
 # History:
-# - 2012-04-03, Hannes Juutilainen, first version
+# 2012-04-10, Hannes Juutilainen
+# - Added support for checking multiple browsers
+# - Changes in output formatting 
+# 2012-04-03, Hannes Juutilainen
+# - First version
 # ================================================================================
 
+# ================================================================================
+# Apps that need to be checked for the LSEnvironment key
+# If you need to check additional paths, add them here
+# ================================================================================
+APPLICATIONS_TO_CHECK=(
+"/Applications/Safari.app"
+"/Applications/Firefox.app"
+"/Applications/Google Chrome.app"
+"/Applications/Opera.app"
+)
+
+SCAN_RESULTS=0
+
+# ================================================================================
 # Check for root
+# ================================================================================
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root" 2>&1
     exit 1
 fi
 
+
 # ================================================================================
-echo "Checking /Applications/Safari.app/Contents/Info.plist for LSEnvironment key"
+echo "Checking for LSEnvironment key in application bundles"
 # ================================================================================
-defaults read /Applications/Safari.app/Contents/Info LSEnvironment > /dev/null 2>&1
-if [[ $? -eq 0 ]]; then
-	printf "%b\n\n" "===> WARNING: Found LSEnvironment in Safari Info.plist"
+for APPLICATION in "${APPLICATIONS_TO_CHECK[@]}"
+do
+	if [[ -e "$APPLICATION/Contents/Info.plist" ]]; then
+		defaults read "$APPLICATION/Contents/Info" LSEnvironment > /dev/null 2>&1
+		if [[ $? -eq 0 ]]; then
+			printf "%b\n" "===> WARNING: Found LSEnvironment in $APPLICATION/Contents/Info.plist"
+			SCAN_RESULTS=1
+		else
+			printf "%b\n" "---> Key not found: $APPLICATION/Contents/Info.plist"
+		fi
+	#else
+		#printf "%b\n" "---> File doesn't exist: $APPLICATION/Contents/Info.plist"
+	fi
+done
+
+
+# ================================================================================
+printf "\n%b\n" "Checking for /Users/Shared/.libgmalloc.dylib"
+# ================================================================================
+if [[ -e /Users/Shared/.libgmalloc.dylib ]]; then
+	printf "%b\n" "===> WARNING: Found /Users/Shared/.libgmalloc.dylib"
+	SCAN_RESULTS=1
 else
-	printf "%b\n\n" "---> Not found"
+	printf "%b\n" "---> File doesn't exist"
 fi
 
 
 # ================================================================================
-echo "Checking if /Users/Shared/.libgmalloc.dylib exists"
-# ================================================================================
-if [[ -f /Users/Shared/.libgmalloc.dylib ]]; then
-	printf "%b\n\n" "===> WARNING: Found /Users/Shared/.libgmalloc.dylib"
-else
-	printf "%b\n\n" "---> Not found"
-fi
-
-
-# ================================================================================
-echo "Checking /Users/*/.MacOSX/environment"
+printf "\n%b\n" "Checking for DYLD_INSERT_LIBRARIES key in /Users/*/.MacOSX/environment.plist"
 # ================================================================================
 shopt -s nullglob
 USER_HOMES=/Users/*
 for f in $USER_HOMES
 do
-	echo "---> Checking $f/.MacOSX/environment.plist"
 	if [[ -f $f/.MacOSX/environment.plist ]]; then
 		defaults read $f/.MacOSX/environment DYLD_INSERT_LIBRARIES > /dev/null 2>&1
 		if [[ $? -eq 0 ]]; then
 			printf "%b\n" "===> WARNING: Found DYLD_INSERT_LIBRARIES key in $f/.MacOSX/environment"
+			SCAN_RESULTS=1
 		fi
+	else
+		printf "%b\n" "---> File doesn't exist in $f/.MacOSX/environment.plist"
 	fi
 done
 shopt -u nullglob
-printf "%b\n\n" "---> Done"
+printf "%b\n" "---> Done"
+
+
+# ================================================================================
+printf "\n%b" "Results: "
+# ================================================================================
+if [[ $SCAN_RESULTS -ne 0 ]]; then
+    printf "%b\n\n" "WARNING: System tested positive on at least one of the tests."
+else
+	printf "%b\n\n" "System is clean."
+fi
 
 exit 0
