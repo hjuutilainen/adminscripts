ec7f7ce6cf40b2f7cef742236f9dc702b1f03889
Hannes Juutilainen <hjuutilainen@mac.com>
2012-04-17 13:58:41 +0300

    Added support for specifying multiple branches as arguments

--- reposado-addNewProducts.sh
+++ reposado-addNewProducts.sh
@@ -9,7 +9,7 @@
 REPOUTIL="/var/git/reposado/code/repoutil"
 
 function usage () {
-	echo "Usage: $0 <branch-name>"
+	echo "Usage: $0 <branch1> <branch2> ..."
 	exit
 }
 
@@ -37,12 +37,20 @@ echo ""
 
 # Ask for confirmation
 while true; do
-    read -p "Add products to catalog $1? [y]n: " yn
+	if [ $# -gt 1 ]; then
+		read -p "Add products to catalog $1? [y]n: " yn
+	else
+		read -p "Add products to catalogs: $*? [y]n: " yn
+	fi
     case $yn in
         [Yy]* ) 
 	    newProducts=( `$REPOUTIL --products | grep -e " \[\] \$" | awk '{print $1}'`)
-	    echo "$REPOUTIL --add-products ${newProducts[@]} $1"
-	    $REPOUTIL --add-products ${newProducts[@]} $1
+	    while [ "$1" != "" ]; do
+			echo "$REPOUTIL --add-products ${newProducts[@]} $1"
+			$REPOUTIL --add-products ${newProducts[@]} $1
+			echo ""
+			shift
+	    done
 	    break;;
         [Nn]* )
 	    exit;;
