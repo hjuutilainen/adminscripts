b05b8dcad1455129ebf92d8766480bae8b979a3c
Hannes Juutilainen <hjuutilainen@mac.com>
2012-05-30 09:32:42 +0300

    Added a new script to disable system wide automatic updates for Google Chrome.
    new file:   chrome-disable-autoupdates.py

---
+++ chrome-disable-autoupdates.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+chrome-disable-autoupdates.py
+
+This script disables system wide automatic updates for Google Chrome. It can 
+optionally remove the whole Keystone and its ticket store too. It should work 
+for Chrome versions 18 and later.
+
+Created by Hannes Juutilainen, hjuutilainen@mac.com
+
+History:
+2012-05-30, Hannes Juutilainen
+- First version
+
+"""
+
+import sys
+import os
+import getopt
+import subprocess
+import plistlib
+
+# =========================================
+# Set this to 'True'
+# to automatically remove the whole Keystone
+# =========================================
+removeKeystone = False
+
+chromePath = "/Applications/Google Chrome.app"
+infoPlistPath = os.path.realpath(os.path.join(chromePath, 'Contents/Info.plist'))
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def chromeIsInstalled():
+    """Check if Chrome is installed"""
+    if os.path.exists(chromePath):
+        return True
+    else:
+        return False
+
+
+def chromeVersion():
+    """Returns Chrome version"""
+    infoPlist = plistlib.readPlist(infoPlistPath)
+    bundleShortVersion = infoPlist["CFBundleShortVersionString"]
+    return bundleShortVersion
+
+
+def chromeKSProductID():
+    """Returns KSProductID from Chrome Info.plist"""
+    infoPlist = plistlib.readPlist(infoPlistPath)
+    KSProductID = infoPlist["KSProductID"]
+    return KSProductID
+
+
+def keystoneRegistrationFrameworkPath():
+    """Returns KeystoneRegistration.framework path"""
+    keystoneRegistration = os.path.join(chromePath, 'Contents/Versions')
+    keystoneRegistration = os.path.join(keystoneRegistration, chromeVersion())
+    keystoneRegistration = os.path.join(keystoneRegistration, 'Google Chrome Framework.framework')
+    keystoneRegistration = os.path.join(keystoneRegistration, 'Frameworks/KeystoneRegistration.framework')
+    return keystoneRegistration
+
+    
+def keystoneNuke():
+    """Nuke the installed Keystone"""
+    installScript = os.path.join(keystoneRegistrationFrameworkPath(), 'Resources/install.py')
+    if os.path.exists(installScript):
+        retcode = subprocess.call([installScript, "--nuke"])
+        if retcode == 0:
+            return True
+        else:
+            return False
+    else:
+        print >> sys.stderr, "Error: KeystoneRegistration.framework not found"
+        return False
+
+
+def removeChromeFromKeystone():
+    """Removes Chrome from Keystone"""
+    ksadmin = "/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/MacOS/ksadmin"
+    ksadminProcess = [  ksadmin, '--delete', '--productid',  chromeKSProductID()]
+    retcode = subprocess.call(ksadminProcess)
+    if retcode == 0:
+        return True
+    else:
+        return False
+
+
+def printUsage():
+    print "Options: "
+    print "  [ -c | --chromeonly   ]      Only remove Chrome ticket (default)"
+    print "  [ -k | --keystoneNuke ]      Remove the whole Keystone"
+    print "  [ -h | --help         ]      Print this message"
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            longArgs = ["help", "keystoneNuke", "chromeonly"]
+            opts, args = getopt.getopt(argv[1:], "hkc", longArgs)
+        except getopt.error, msg:
+            printUsage()
+            return 1
+        
+        global removeKeystone
+        for option, value in opts:
+            if option in ("-c", "--chromeonly"):
+                removeKeystone = False
+            if option in ("-k", "--keystoneNuke"):
+                removeKeystone = True
+            if option in ("-h", "--help"):
+                printUsage()
+                return 1
+        
+        # Check for root
+        if os.geteuid() != 0:
+            print >> sys.stderr, "This script must be run as root"
+            return 1
+        
+        # Check that Chrome is actually installed
+        if not chromeIsInstalled():
+            print >> sys.stderr, "Error: Chrome is not installed on this computer"
+            return 1
+        
+        if removeKeystone:
+            if keystoneNuke():
+                print "Keystone removed"
+                return 0
+            else:
+                print >> sys.stderr, "Error: Keystone nuke failed"
+                return 1
+        
+        if removeChromeFromKeystone():
+            print "Removed Chrome from Keystone"
+            return 0
+        else:
+            print >> sys.stderr, "Error: Failed to remove Chrome from Keystone"
+            return 1
+    
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+
+if __name__ == "__main__":
+    sys.exit(main())
