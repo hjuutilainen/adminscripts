fb373d3c37e01769d9e49bf8336b027016a44eaa
cintom00 <tom.cinbis@unibas.ch>
2017-11-27 17:09:32 +0100

    Ported shell script to python
    Now we do NOT need a python script and a shell script. Everything is
    contained inside one pythons script.

--- check-10.13-high-sierra-compatibility.py
+++ check-10.13-high-sierra-compatibility.py
@@ -18,34 +18,36 @@
 # 1 = High Sierra is not supported
 #
 #
-# Modified for High Sierra by Tom Cinbis
+# Ported to Python for High Sierra by Tom Cinbis
 # https://github.com/tcinbis/adminscripts
 #
 # Original script by Hannes Juutilainen <hjuutilainen@mac.com>
 # https://github.com/hjuutilainen/adminscripts
 #
-# Checker created by Laurent Pertois @Jamf
+# Checker shell script created by Laurent Pertois
 # https://github.com/laurentpertois/High-Sierra-Compatibility-Checker
 #
 # ================================================================================
 
-import sys
-import subprocess
 import os
-import re
+import platform
 import plistlib
-from distutils.version import StrictVersion
+import re
+import subprocess
+import sys
 
 # ================================================================================
 # Start configuration
 # ================================================================================
 
 # Set this to False if you don't want any output, just the exit codes
+
 verbose = True
 
 # Set this to True if you want to add "high_sierra_supported" custom conditional to
 # /Library/Managed Installs/ConditionalItems.plist
 update_munki_conditional_items = True
+
 
 # ================================================================================
 # End configuration
@@ -67,7 +69,8 @@ def conditional_items_path():
 
     # Make sure we're outputting our information to "ConditionalItems.plist"
     if managed_installs_dir:
-        return os.path.join(managed_installs_dir, 'ConditionalItems.plist')
+        conItemsPath = os.path.join(managed_installs_dir.decode('ascii'), 'ConditionalItems.plist')
+        return conItemsPath
     else:
         # Munki default
         return "/Library/Managed Installs/ConditionalItems.plist"
@@ -84,14 +87,46 @@ def munki_installed():
 
 
 def run_script():
-    cmd = ["/bin/sh", "/usr/local/munki/conditions/High_Sierra_Compatibility_Checker.sh"]
-    p = subprocess.Popen(cmd, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (results, err) = p.communicate()
-    for result in results.split():
-        if result == "True":
-            print("System is compatible")
-            return True
-    return False
+    version = platform.mac_ver()[0]
+    majorVersion = int(str(version).split('.')[1])
+    minorVersion = int(str(version).split('.')[2])
+
+    supported = False
+
+    if (majorVersion >= 8 and minorVersion < 13) or (majorVersion == 7 and minorVersion == 5):
+        ramCmd = ['/usr/sbin/sysctl', '-n', 'hw.memsize']
+        ram = int(subprocess.run(ramCmd, stdout=subprocess.PIPE).stdout.decode('ascii')) / 1024 / 1024 / 1024
+
+        minimumRam = 2
+        minimumSpace = 9
+
+        freeSpaceCmd = [
+            '/usr/sbin/diskutil info / | awk -F\'[()]\' \'/Free Space|Available Space/ {print $2}\'| sed -e \'s/\ Bytes//\'']
+        freeSpace = int(int(subprocess.run(freeSpaceCmd, stdout=subprocess.PIPE, shell=True).stdout.decode(
+            'ascii')) / 1024 / 1024 / 1024)
+
+        modelIdentifierCmd = ['/usr/sbin/sysctl -n hw.model']
+        modelIdentifier = str(
+            subprocess.run(modelIdentifierCmd, stdout=subprocess.PIPE, shell=True).stdout.decode('ascii')).replace('\n',
+                                                                                                                   '')
+        modelnameCmd = 'echo \"' + modelIdentifier + '\" | sed s/[^0-9,]//g | awk -F, \'{print $1}\''
+        modelname = re.sub(r'[0-9,]', '', modelIdentifier)
+        modelVersion = int(re.sub(r',.*', '', re.sub(r'[^0-9,]', '', modelIdentifier)))
+
+        if modelname == "iMac" and modelVersion >= 10 and ram >= minimumRam and freeSpace >= minimumSpace:
+            supported = True
+        elif modelname == "Macmini" and modelVersion >= 4 and ram >= minimumRam and freeSpace >= minimumSpace:
+            supported = True
+        elif modelname == "MacPro" and modelVersion >= 5 and ram >= minimumRam and freeSpace >= minimumSpace:
+            supported = True
+        elif modelname == "MacBook" and modelVersion >= 6 and ram >= minimumRam and freeSpace >= minimumSpace:
+            supported = True
+        elif modelname == "MacBookAir" and modelVersion >= 3 and ram >= minimumRam and freeSpace >= minimumSpace:
+            supported = True
+        elif modelname == "MacBookPro" and modelVersion >= 7 and ram >= minimumRam and freeSpace >= minimumSpace:
+            supported = True
+
+    return supported
 
 
 def append_conditional_items(dictionary):
@@ -102,7 +137,6 @@ def append_conditional_items(dictionary):
     else:
         output_dict = dictionary
     plistlib.writePlist(output_dict, current_conditional_items_path)
-    pass
 
 
 def main(argv=None):
@@ -118,6 +152,7 @@ def main(argv=None):
     # Update "ConditionalItems.plist" if munki is installed
     if munki_installed() and update_munki_conditional_items:
         append_conditional_items(high_sierra_supported_dict)
+
     # Exit codes:
     # 0 = High Sierra is supported
     # 1 = High Sierra is not supported
